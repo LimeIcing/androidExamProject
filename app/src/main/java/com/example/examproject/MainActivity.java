3320cb6eaebe153a1dadf077015d28cdc2a41b61
Lime Icing <emil577b@stud.kea.dk>
2019-05-31 13:15:05 +0200

    readied implementation for Firebase, picked colours, started work on overview activity

--- app/src/main/java/com/example/examproject/MainActivity.java
+++ app/src/main/java/com/example/examproject/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.examproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate() called");
+        init();
+    }
+
+    private void init(){
+        Log.d(TAG, "Initializing...");
+
+        button = findViewById(R.id.button);
+        final Intent intent = new Intent(this, OverviewActivity.class);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/examproject/OverviewActivity.java
@@ -0,0 +1,23 @@
+package com.example.examproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    private final String TAG = "OverviewActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+        Log.d(TAG, "onCreate() called");
+        init();
+    }
+
+    private void init(){
+        Log.d(TAG, "Initializing...");
+
+    }
+}
