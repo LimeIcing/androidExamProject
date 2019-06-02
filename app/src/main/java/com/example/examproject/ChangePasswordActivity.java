5020500f69c4113c0804c3f921598ee7c3a418a5
Lime Icing <emil577b@stud.kea.dk>
2019-06-03 00:25:40 +0200

    began work on changing password

--- app/src/main/java/com/example/examproject/ChangePasswordActivity.java
+++ app/src/main/java/com/example/examproject/ChangePasswordActivity.java
@@ -4,10 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.examproject.models.Account;
+import com.example.examproject.models.User;
 
 public class ChangePasswordActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final String TAG = "ChangePasswordActivity";
+
+    // region UI
+    private EditText editTextOldPassword, editTextNewPassword;
+    private Button buttonChangePassword;
+    // endregion
+
+    private User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +32,17 @@ public class ChangePasswordActivity extends AppCompatActivity implements View.On
 
     @Override
     public void onClick(View v) {
-
+        
     }
 
     private void init() {
         Log.d(TAG, "initializing...");
+
+        currentUser = getIntent().getParcelableExtra("user");
+
+        editTextOldPassword = findViewById(R.id.textViewOldPassword);
+        editTextNewPassword = findViewById(R.id.textViewNewPassword);
+        buttonChangePassword = findViewById(R.id.buttonChangePassword);
+        buttonChangePassword.setOnClickListener(this);
     }
 }
